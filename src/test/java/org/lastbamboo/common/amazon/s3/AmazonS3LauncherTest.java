f8b883920093efdba48cee081858c54765706ab4
adamfisk <a@littleshoot.org>
2008-02-12 16:16:34 +0000

    tweaks
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/amazon/s3@2373 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/test/java/org/lastbamboo/common/amazon/s3/AmazonS3LauncherTest.java
+++ src/test/java/org/lastbamboo/common/amazon/s3/AmazonS3LauncherTest.java
@@ -1,6 +1,7 @@
 package org.lastbamboo.common.amazon.s3;
 
 import org.junit.Test;
+import org.lastbamboo.common.amazon.stack.AmazonWsUtils;
 
 
 public class AmazonS3LauncherTest
@@ -8,7 +9,10 @@ public class AmazonS3LauncherTest
 
     @Test public void testLaunching() throws Exception
         {
-    
+        if (!AmazonWsUtils.hasPropsFile()) 
+            {
+            return;
+            }
         Launcher.main(new String[] {"-h"});
         //System.out.println("\n\n");
         
